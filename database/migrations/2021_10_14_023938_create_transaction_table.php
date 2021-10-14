ba747afbab02c179ae853e18fc63feadc7b73925
Rafael Morales <rmorales@utem.cl>
2021-10-13 23:46:57 -0300

    Modelo y Migracion para Transaccion

---
+++ database/migrations/2021_10_14_023938_create_transaction_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Client;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Client::class);
+            $table->boolean('wasPayed');
+            $table->boolean('wasReceived');
+            $table->text('extra');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
