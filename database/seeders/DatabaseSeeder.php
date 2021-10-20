47203e8a23a3325742eaaed0a725c3b64c5bd222
Rafael Morales <rmorales@utem.cl>
2021-10-20 17:56:53 -0300

    Implemented Seeding of Orders and Products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Client;
+use App\Models\Order;
+use App\Models\Product;
 use App\Models\Transaction;
 use Illuminate\Database\Seeder;
 
@@ -18,5 +20,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         Client::factory(10)->create();
         Transaction::factory(10)->create();
+        Product::factory(10)->create();
+        Order::factory(10)->create();
     }
 }
