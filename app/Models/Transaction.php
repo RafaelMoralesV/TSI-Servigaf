8862084b25a98ad36b9b5ab4f9d88ee7985ba200
Rafael Morales <rmorales@utem.cl>
2021-10-19 22:19:14 -0300

    Many-to-Many relationship

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function client(): BelongsToMany {
+        return $this->belongsToMany(Client::class);
+    }
 }
