337618694ce5fbe99f397b7647d2f6eee15ce733
Rafael Morales <rmorales@utem.cl>
2021-10-20 17:46:20 -0300

    One-To-Many relationship fixed

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,13 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
     use HasFactory;
 
-    public function client(): BelongsToMany {
-        return $this->belongsToMany(Client::class);
+    public function client(): BelongsTo {
+        return $this->belongsTo(Client::class);
     }
 }
