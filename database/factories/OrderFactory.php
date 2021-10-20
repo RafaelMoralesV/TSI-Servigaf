e16c2a69136d73d7e8b23a16244b66bb16ba2ceb
Rafael Morales <rmorales@utem.cl>
2021-10-20 17:56:29 -0300

    Created OrderFactory

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Order::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'transaction_id' => Transaction::get()->random(),
+            'product_id' => Product::get()->random(),
+            'amount' => $this->faker->numberBetween(0, 30),
+        ];
+    }
+}
