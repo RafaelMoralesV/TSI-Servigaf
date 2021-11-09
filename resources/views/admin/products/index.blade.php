e9c9e97f00f83074e98c725648bb4dd05997ac4f
RafaelMoralesV <rmorales@utem.cl>
2021-11-08 21:33:56 -0300

    Changing views to match rest of the app

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,28 +1,39 @@
-@extends ('layouts.guest-navigation')
-
-@section('content')
-    <table class="table-auto">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Category</th>
-        </tr>
-        </thead>
-        <tbody>
-        @forelse($products as $product)
-            <tr>
-                <td>{{ $product->id }}</td>
-                <td>{{ $product->name }}</td>
-                <td>{{ $product->category }}</td>
-            </tr>
-        @empty
-            <tr>
-                <td>None</td>
-                <td>None</td>
-                <td>None</td>
-            </tr>
-        @endforelse
-        </tbody>
-    </table>
-@endsection
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Products') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <table class="table-auto">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse($products as $product)
+                            <tr>
+                                <td>{{ $product->id }}</td>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->category }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td>None</td>
+                                <td>None</td>
+                                <td>None</td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
