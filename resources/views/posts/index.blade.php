8e03aabbb480378834b17df778c66fa15acf6380
cbandera1 <cbandera@utem.cl>
2021-11-12 21:09:05 -0300

    anadido carpeta public logos

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -194,7 +194,7 @@ Alternatively if you want to just have a single hero
 
     <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">This template is inspired by the stunning nordic minamalist design - in particular:
             <br>
-            
+            <img src= "{{ asset('logos/Logo-trotter.jpg') }}">
     </div>
 
         <p class="mb-8">Lorem ipsum dolor sit amet, consectetur <a href="#">random link</a> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Vitae aliquet nec ullamcorper sit. Nullam eget felis eget nunc lobortis mattis aliquam. In est ante in nibh mauris. Egestas congue quisque egestas diam in. Facilisi nullam vehicula ipsum a arcu. Nec nam aliquam sem et tortor consequat. Eget mi proin sed libero enim sed faucibus turpis in. Hac habitasse platea dictumst quisque. In aliquam sem fringilla ut. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Accumsan lacus vel facilisis volutpat est velit egestas dui id. At tempor commodo ullamcorper a. Volutpat commodo sed egestas egestas fringilla. Vitae congue eu consequat ac.</p>
