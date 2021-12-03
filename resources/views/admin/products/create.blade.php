68178021517d6755736a9890dbf06db46b8c564f
cbandera1 <cbandera@utem.cl>
2021-12-02 22:12:52 -0300

    ARREGLADO SI

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -52,7 +52,7 @@
                             <x-input id="stock" class="block mt-1 w-full"
                                      type="number"
                                      name="stock"
-                                     :value="old('stock') ?? $product->stock"
+                                     :value="old('stock') ?? 0"
                                      required></x-input>
                         </div>
 
