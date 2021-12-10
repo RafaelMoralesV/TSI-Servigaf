7b93e996f8bb4aa7888098a5dbe80e8db0e87a6d
RafaelMoralesV <rmorales@utem.cl>
2021-12-10 20:17:39 -0300

    Error evitado, por favor re migrar

--- database/migrations/2021_12_09_192444_added_categories_field_to_products.php
+++ database/migrations/2021_12_09_192444_added_categories_field_to_products.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,9 +18,9 @@ class AddedCategoriesFieldToProducts extends Migration
             $table->dropColumn([
                 'category'
             ]);
-            $table->foreignIdFor(\App\Models\Category::class)
+            $table->foreignIdFor(Category::class)
                 ->nullable()
-                ->constrained()->onDelete('cascade');
+                ->constrained()->onDelete('set null');
         });
     }
 
