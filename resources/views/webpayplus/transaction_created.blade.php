b5d81ebd97b1d7a8b947ea91323aba831a14609a
cbandera1 <cbandera@utem.cl>
2021-12-13 00:10:18 -0300

    Cambio view de transaccion creada

--- resources/views/webpayplus/transaction_created.blade.php
+++ resources/views/webpayplus/transaction_created.blade.php
@@ -3,16 +3,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Estas en Transbank/create_transaction
-                    <br>
-                    {{ $resp->token }}
-                    <br>
-                    {{ $resp->url }}
-
-                    <form action="{{ $resp->getUrl() }}" method="POST">
+                    <div class="flex justify-center flex-col">
+                    <h1 class="text-4xl font-mono mx-auto">Continuar a:</h1>
+                    <form class="mx-auto" action="{{ $resp->getUrl() }}" method="POST">
                         <input type="hidden" name="token_ws" value="{{ $resp->getToken() }}">
-                        <button type="submit">{{ __('Ir a Webpay') }}</button>
+                        <input type="image" src="{{ asset('logos/webpay-cl.jpg') }}" alt="{{ __('Ir a Webpay') }}" ></input>
                     </form>
+                </div>
                 </div>
             </div>
         </div>
